a1445a9d32ba5f0c21798fcdb99978e75939c901
Jonathan Corea <jacorea410@gmail.com>
2021-11-17 22:24:52 -0800

    Create file layout for initialization: components, layout, meals,cart. add initial styling. Create HeaderCartButton, CartIcon, and Header.

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React, {Fragment} from 'react'
+import meals from  '../../.././assets/meals.jpg'
+
+//components
+import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
+
+
+//styling
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Meals2Go</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt="dinning table full of dinner items" />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
