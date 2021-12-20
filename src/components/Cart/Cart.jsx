548e0b70ee2180df78d39d3c40d8a85e5cb7e1b6
Jonathan Corea <jacorea410@gmail.com>
2021-12-20 15:02:03 -0800

    add logic to Store Provider to update multimple submissions of food items.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import Modal from '../UI/Modal/Modal';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem/CartItem';
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext)
+
+    //total Amount
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = id => {};
+
+    const cartItemAddHandler = item => {};
+
+
+
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {cartCtx.items.map((item)=><CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}/>)}
+        </ul>
+    )
+
     return (
         <Modal onHideCart={props.onHideCart}>
-            <ul className={classes['cart-items']}>
-                <li>cartItems</li>
-            </ul>
+            {cartItems}
             <div className={classes.total}>
-                <span>total</span>
-                <span>$35.00</span>
+                <span>Total Amount</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button> }
             </div>
         </Modal>
     )
